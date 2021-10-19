1777e50039e58e922fe790e42bf9c114c10c5fc0
tridoan <tridoan1998@gmail.com>
2021-10-18 17:27:30 -0700

    completed front end

---
+++ src/Form.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class Form extends Component {
+    render() {
+    return (
+        <div className="App"> 
+        <Form />
+
+         </div>
+
+    ) 
+
+    }
+}
+
+export default Form 
